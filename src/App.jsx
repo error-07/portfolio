16423d8defc6ff3eabf5d90a420d32f9f8a2f3f9
error-07 <66213258+error-07@users.noreply.github.com>
2025-07-08 16:54:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,17 @@ import PageWrapper from "./components/PageWrapper";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-r from-indigo-900 via-blue-700 to-purple-900 text-white font-poppins">
-      <Router>
-        <Navbar />
-        <PageWrapper>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/projects" element={<Projects />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </PageWrapper>
-      </Router>
-    </div>
+    <Router>
+      <Navbar />
+      <PageWrapper>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </PageWrapper>
+    </Router>
   );
 }
 
